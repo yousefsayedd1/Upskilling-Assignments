3793c13764682b185ba738becf4365b361d9f657
Yousef Sayed <95933671+yousefsayedd1@users.noreply.github.com>
2024-10-14 07:00:28 +0300

    Add files via upload

---
+++ Day-1/Upskilling/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace Upskilling
+{
+    public class BankAccount
+    {
+        public int AccountNumber { get; }
+        public string AccountHolderName { get; set; }
+        private decimal _balance;
+        public decimal Balance
+        {
+            get { return _balance; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception();
+                }
+                _balance = value;
+            }
+        }
+        public BankAccount(int acountNumber)
+        {
+            AccountNumber = acountNumber;
+        }
+        public void Deposit(decimal depositeAmount)
+        {
+            Balance += depositeAmount;
+        }
+        public void Withdraw(decimal withdrawAmount)
+        {
+            Balance -= withdrawAmount;
+        }
+    }
+}
---
+++ Day-1/Upskilling/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+
+namespace Upskilling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+}
