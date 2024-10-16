e73b4d36a3f2b0ba44b01a01ffc465031ed161a2
Yousef Sayed <95933671+yousefsayedd1@users.noreply.github.com>
2024-10-16 03:28:04 +0300

    Add files via upload

---
+++ Day1/Product.cs
@@ -0,0 +1,48 @@
+﻿namespace Upskilling
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        private decimal _price;
+        public decimal Price
+        {
+            get { return Price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+                _price = value;
+            }
+        }
+        public int StockQuantity
+        {
+            get
+            {
+                return StockQuantity;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception();
+                }
+                Price = value;
+            }
+        }
+        public bool IsAvailable
+        {
+            get { return IsAvailable; }
+            set
+            {
+                IsAvailable = StockQuantity > 0;
+            }
+        }
+        public void UpdateStock(int stockQuantity)
+        {
+            StockQuantity = stockQuantity;
+            IsAvailable |= StockQuantity > 0;
+        }
+    }
+}
